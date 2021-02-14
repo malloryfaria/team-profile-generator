e6a3aee3bca70ea02c455f290707944a53bb9955
Mallory <mallory.faria@gmail.com>
2021-02-13 22:33:41 -0500

    added more to the file skeletons

---
+++ Index.js
@@ -0,0 +1,8 @@
+// Required packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+// Required module exports
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
